dc2f48b0bf2fad7e1171cba5472ba9f7e559d6bf
Lanre Adelowo <yo@lanre.wtf>
2023-05-12 11:42:12 +0100

    get initial version of middleware working

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/ayinke-llc/otito-go-middleware
 
 go 1.19
 
-require github.com/ayinke-llc/otito-go v0.0.0-20230511120341-9e14616578dc
+require (
+	github.com/ayinke-llc/otito-go v0.0.0-20230512100532-ab7122eadcc8
+	github.com/stretchr/testify v1.8.2
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- middleware.go
+++ middleware.go
@@ -1,6 +1,9 @@
 package otito
 
 import (
+	"bytes"
+	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -18,6 +21,8 @@ type MiddlewareConfig struct {
 	client                           *otito.APIClient
 	numberOfMessagesBeforePublishing int64
 	ipStrategy                       IPStrategy
+	ingesterURL                      string
+	apiKey                           string
 }
 
 type MessageHTTPDefinition struct {
@@ -33,6 +38,24 @@ type Message struct {
 	IPAddress string                `json:"ip_address"`
 }
 
+func New(apiKey string, ingesterURL string, fn AppIDFn, numberOfMessagesBeforePublishing int64, ipstrat IPStrategy) *MessageStore {
+	cfg := otito.NewConfiguration()
+
+	client := otito.NewAPIClient(cfg)
+
+	return &MessageStore{
+		messages: make([]Message, 0),
+		mutex:    sync.RWMutex{},
+		config: MiddlewareConfig{
+			appIDFn:                          fn,
+			numberOfMessagesBeforePublishing: numberOfMessagesBeforePublishing,
+			ipStrategy:                       ipstrat,
+			client:                           client,
+			apiKey:                           apiKey,
+		},
+	}
+}
+
 type MessageStore struct {
 	messages []Message
 	mutex    sync.RWMutex
@@ -40,6 +63,51 @@ type MessageStore struct {
 }
 
 func (m *MessageStore) flush() error {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+
+	ctx := context.WithValue(context.Background(), otito.ContextAPIKeys, map[string]otito.APIKey{
+		"ApiKeyAuth": {
+			Key:    m.config.apiKey,
+			Prefix: "Bearer",
+		},
+	})
+
+	msg := otito.NewServerMessageRequest()
+
+	for _, v := range m.messages {
+		c := int32(v.CreatedAt)
+
+		msg.Messages = append(msg.Messages, otito.ServerMessageRequestMessagesInner{
+			App:       &v.App,
+			CreatedAt: &c,
+			IpAddress: &v.IPAddress,
+			Request: &otito.ServerMessageHTTPDefinition{
+				Body:   &v.Request.Body,
+				Header: &v.Request.Header,
+			},
+			Response: &otito.ServerMessageHTTPDefinition{
+				Body:   &v.Response.Body,
+				Header: &v.Response.Header,
+			},
+		})
+	}
+
+	status, resp, err := m.config.client.MessageApi.MessagesPost(ctx).
+		Message(*msg).Execute()
+	if err != nil {
+		panic("oops")
+	}
+
+	if resp.StatusCode > http.StatusContinue {
+		panic("oops")
+	}
+
+	fmt.Println(status)
+	if !status.GetStatus() {
+		panic("oops")
+	}
+
 	return nil
 }
 
@@ -66,22 +134,35 @@ func (m *MessageStore) Handler(next http.Handler) http.Handler {
 
 		r.Body = io.NopCloser(r.Body)
 
+		b := bytes.NewBuffer(nil)
+
+		_, err := io.Copy(b, r.Body)
+		if err != nil {
+			// remove this rubbish
+			// TODO(adelowo): please ffs
+			panic("oops")
+		}
+
 		w.WriteHeader(rec.Code)
 
 		buf := rec.Body.Bytes()
 
 		w.Write(buf)
 
-		msg := &Message{
+		msg := Message{
 			CreatedAt: time.Now().Unix(),
 			App:       m.config.appIDFn(r),
 			IPAddress: getIP(r, m.config.ipStrategy),
 			Request: MessageHTTPDefinition{
 				Header: r.Header.Clone(),
+				Body:   b.String(),
+			},
+			Response: MessageHTTPDefinition{
+				Header: rec.Header().Clone(),
+				Body:   string(buf),
 			},
 		}
 
-		msg.Response.Header = rec.Header().Clone()
-		msg.Response.Body = string(buf)
+		m.add(msg)
 	})
 }
